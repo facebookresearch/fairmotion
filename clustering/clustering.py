882b46c767e5445f8b6a7171fc7cb21f27c1f86a
Deepak Gopinath <dgopinath@fb.com>
2019-12-12 08:06:59 -0800

    Incorporate change in features format while clustering

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -1,25 +1,34 @@
+import argparse
+import os
 from sklearn.cluster import KMeans
 from collections import defaultdict
 
-def main():
+
+def main(args):
     features = []
     names = []
-    with open("/private/home/dgopinath/data/pfnn_locomation_bvh_split_features/features.tsv") as f:
+    with open(args.features) as f:
         for line in f:
-            features.append([int(x) for x in line.strip().split("\t")])
-    with open("/private/home/dgopinath/data/pfnn_locomation_bvh_split_features/labels.tsv") as f:
-        for line in f:
-            names.append(line.strip())
+            line = line.strip()
+            names.append(line.split(":")[0])
+            features.append([float(x) for x in line.split(":")[-1].split("\t")])        
     kmeans = KMeans(20).fit(features)
     clusters = defaultdict(list)
     print(kmeans.labels_)
     print(len(features))
     for num, label in enumerate(kmeans.labels_):
         clusters[label].append(names[num])
-    with open("/private/home/dgopinath/data/pfnn_locomation_bvh_split_features/clusters.tsv", "w") as f:
+    with open(os.path.join(args.output_folder, "clusters.tsv"), "w",) as f:
         for cluster in clusters:
             for name in clusters[cluster]:
-                f.write(f"{cluster}:{name}\n")
+                f.write(str(cluster) + ":" + str(name) + "\n")
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Cluster features with kMeans")
+    parser.add_argument("--features", type=str, help="Features tsv file")
+    parser.add_argument(
+        "--output-folder", type=str, help="Folder to output clusters.csv"
+    )
+    args = parser.parse_args()
+    main(args)
