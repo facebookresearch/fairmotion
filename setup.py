e1906c8d4a6b4f0100c3a657bac46b095b980168
Deepak Gopinath <dgopinath@fb.com>
2019-12-20 15:00:38 -0800

    Initial scaffolding for mocap library

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mocap_processing',
+      version='0.1',
+      description='',
+      url='https://github.com/fairinternal/mocap_processing',
+      author='FAIR Pittsburgh',
+      author_email='dgopinath@fb.com',
+      packages=['mocap_processing'],
+      zip_safe=False)
