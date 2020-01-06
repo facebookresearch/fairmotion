02d73588e20a628019212fa8777cf0e025734d6a
Deepak Gopinath <dgopinath@fb.com>
2020-01-05 19:20:48 -0800

    Add attention layer to LSTM seq2seq model

--- mocap_processing/models/seq2seq.py
+++ mocap_processing/models/seq2seq.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import random
 
 
@@ -10,8 +11,8 @@ class LSTMEncoder(nn.Module):
         self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True)
 
     def forward(self, input):
-        _, (lstm_hidden, lstm_cell) = self.lstm(input)
-        return lstm_hidden, lstm_cell
+        outputs, (lstm_hidden, lstm_cell) = self.lstm(input)
+        return lstm_hidden, lstm_cell, outputs
 
 
 class DecoderStep(nn.Module):
@@ -22,7 +23,7 @@ class DecoderStep(nn.Module):
         self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim)
         self.out = nn.Linear(hidden_dim, output_dim)
 
-    def forward(self, input, hidden=None, cell=None):
+    def forward(self, input, hidden=None, cell=None, encoder_outputs=None):
         if (hidden is None) and (cell is None):
             output, (hidden, cell) = self.lstm(input)
         else:
@@ -35,10 +36,10 @@ class DecoderStep(nn.Module):
 class LSTMDecoder(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim, device='cuda'):
         super(LSTMDecoder, self).__init__()
-        self.decoder = DecoderStep(input_dim, output_dim, hidden_dim)
+        self.decoder_step = DecoderStep(input_dim, output_dim, hidden_dim)
         self.device = device
 
-    def forward(self, trg, hidden=None, cell=None, teacher_forcing_ratio=0.5):
+    def forward(self, trg, hidden=None, cell=None, encoder_outputs=None, teacher_forcing_ratio=0.5):
         trg = trg.transpose(0, 1)
         max_len = trg.shape[0]
         batch_size = trg.shape[1]
@@ -47,13 +48,56 @@ class LSTMDecoder(nn.Module):
         outputs = torch.zeros(max_len, batch_size, self.decoder.input_dim).to(self.device)
         for t in range(max_len):
             input = input.unsqueeze(0)
-            output, hidden, cell = self.decoder(input, hidden, cell)
+            if (hidden is None) and (cell is None):
+                output, (hidden, cell) = self.lstm(input)
+            else:
+                output, (hidden, cell) = self.lstm(input, (hidden, cell))
+            output, hidden, cell = self.decoder_step(input, hidden, cell)
             outputs[t] = output
             teacher_force = random.random() < teacher_forcing_ratio
             input = trg[t] if teacher_force else output
 
         outputs = outputs.transpose(0, 1)
         return outputs
+
+
+class DecoderStepWithAttention(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, source_length, device="cuda"):
+        super(DecoderStepWithAttention, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.source_length = source_length
+        self.device = device
+
+        self.attn = nn.Linear(self.hidden_dim + self.input_dim, self.source_length)
+        self.attn_combine = nn.Linear(self.hidden_dim + self.input_dim, self.input_dim)
+        self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim)
+        self.out = nn.Linear(self.hidden_dim, self.output_dim)
+
+
+    def forward(self, input, hidden, cell, encoder_outputs):
+        attn_weights = F.softmax(self.attn(torch.cat((input, hidden), 2)), dim=2)
+        attn_applied = torch.bmm(attn_weights.transpose(0, 1), encoder_outputs)
+
+        output = torch.cat((input, attn_applied.transpose(0, 1)), 2)
+        output = self.attn_combine(output)
+        output = F.relu(output)
+
+        if (hidden is None) and (cell is None):
+            output, (hidden, cell) = self.lstm(output)
+        else:
+            output, (hidden, cell) = self.lstm(output, (hidden, cell))
+        output = output.squeeze(0)
+        output = self.out(output)
+        return output, hidden, cell
+
+
+class LSTMDecoderWithAttention(LSTMDecoder):
+    def __init__(self, input_dim, output_dim, max_source_length, hidden_dim=128, device='cuda'):
+        super(LSTMDecoderWithAttention, self).__init__(input_dim, output_dim, hidden_dim, device)
+        self.decoder_step = DecoderStepWithAttention(input_dim, output_dim, hidden_dim, max_source_length)
+        self.device = device
 
 
 class Seq2Seq(nn.Module):
@@ -63,6 +107,6 @@ class Seq2Seq(nn.Module):
         self.decoder = decoder
 
     def forward(self, src, trg, teacher_forcing_ratio=0.5):
-        hidden, cell = self.encoder(src)
-        outputs = self.decoder(trg, hidden, cell)
+        hidden, cell, outputs = self.encoder(src)
+        outputs = self.decoder(trg, hidden, cell, outputs)
         return outputs
