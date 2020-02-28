d9ff08604f5eec1e5298e4472d9cc243854947ec
Deepak Gopinath <dgopinath@fb.com>
2020-02-28 11:51:51 -0800

    More clustering and embedding types

--- mocap_processing/tasks/clustering/clustering.py
+++ mocap_processing/tasks/clustering/clustering.py
@@ -7,7 +7,7 @@ import argparse
 import numpy as np
 import os
 from scipy.cluster.hierarchy import dendrogram, linkage
-from sklearn.cluster import AgglomerativeClustering, KMeans, OPTICS, cluster_optics_dbscan
+from sklearn.cluster import AgglomerativeClustering, DBSCAN, KMeans, OPTICS
 from collections import defaultdict
 
 
@@ -35,8 +35,32 @@ def get_ranked_clusters(clusters):
     return ranked_clusters
 
 
+def calculate_cluster_centroids(features, labels):
+    cluster_centroids = defaultdict(lambda: np.zeros(len(features[0])))
+    for num, label in enumerate(labels):
+        cluster_centroids[label] += np.array(features[num])
+    # Average sum of points to get centroids
+    for cluster in cluster_centroids:
+        cluster_centroids[cluster] = cluster_centroids[cluster]/len(cluster_centroids[cluster])
+    return cluster_centroids
+
+
+def run_dbscan_clustering(features, names, args):
+    dbscan = DBSCAN(eps=3, min_samples=2).fit(features)
+    cluster_centroids = calculate_cluster_centroids(features, dbscan.labels_)
+    clusters = defaultdict(list)
+    for num, label in enumerate(dbscan.labels_):
+        clusters[label].append((names[num], calculate_score(cluster_centroids[label], features[num])))
+    return clusters    
+
+
 def run_optics_clustering(features, names, args):
-    optics = OPTICS(min_samples=50, xi=.05, min_cluster_size=.05)
+    optics = OPTICS(min_samples=10).fit(features)
+    cluster_centroids = calculate_cluster_centroids(features, optics.labels_)
+    clusters = defaultdict(list)
+    for num, label in enumerate(optics.labels_):
+        clusters[label].append((names[num], calculate_score(cluster_centroids[label], features[num])))
+    return clusters
 
 
 def run_kmeans_clustering(features, names, args):
@@ -46,38 +70,24 @@ def run_kmeans_clustering(features, names, args):
         score = calculate_score(kmeans.cluster_centers_[label], features[num])
         clusters[label].append((names[num], score))
 
-    ranked_clusters = get_ranked_clusters(clusters)
-    
-    with open(args.output_file, "w") as f:
-        for cluster in ranked_clusters:
-            for (name, rank, score) in ranked_clusters[cluster]:
-                f.write(",".join([str(cluster), str(rank), str(score)]) + ":" + str(name) + "\n")
+    return clusters
 
 
 def run_hierarchical_clustering(features, names, args):
     hierarchical = AgglomerativeClustering(args.num_clusters, linkage=args.linkage).fit(features)
-    cluster_centroids = defaultdict(lambda: np.zeros(len(features[0])))
-    for num, label in enumerate(hierarchical.labels_):
-        cluster_centroids[label] += np.array(features[num])
-    # Average sum of points to get centroids
-    for cluster in cluster_centroids:
-        cluster_centroids[cluster] = cluster_centroids[cluster]/len(cluster_centroids[cluster])
-
+    cluster_centroids = calculate_cluster_centroids(features, hierarchical.labels_)
     clusters = defaultdict(list)
     for num, label in enumerate(hierarchical.labels_):
         clusters[label].append((names[num], calculate_score(cluster_centroids[label], features[num])))
-    
-    ranked_clusters = get_ranked_clusters(clusters)
-    
-    with open(args.output_file, "w") as f:
-        for cluster in ranked_clusters:
-            for (name, rank, score) in ranked_clusters[cluster]:
-                f.write(",".join([str(cluster), str(rank), str(score)]) + ":" + str(name) + "\n")
+
+    return clusters
+
 
 def normalize_features(features):
     X = np.array(features)
     Xmean, Xstd = X.mean(axis=0), X.std(axis=0)
     return (X - Xmean) / (Xstd + 1.0e-8)
+
 
 def main(args):
     features = []
@@ -87,16 +97,27 @@ def main(args):
             line = line.strip()
             names.append(line.split(":")[0])
             features.append([float(x) for x in line.split(":")[-1].split("\t")])
+
     if 0.0 < args.clip_features < 100.0:
         np.percentile(features, args.clip_features, axis=0, overwrite_input=True)
     if args.normalize_features:
         features = normalize_features(features)
+
     if args.type == "kmeans":
-        run_kmeans_clustering(features, names, args)
+        clusters = run_kmeans_clustering(features, names, args)
     elif args.type == "hierarchical":
-        run_hierarchical_clustering(features, names, args)
+        clusters =  run_hierarchical_clustering(features, names, args)
     elif args.type == "optics":
-        run_optics_clustering(features, names, args)
+        clusters = run_optics_clustering(features, names, args)
+    elif args.type == "dbscan":
+        clusters = run_dbscan_clustering(features, names, args)
+    
+    ranked_clusters = get_ranked_clusters(clusters)
+    
+    with open(args.output_file, "w") as f:
+        for cluster in ranked_clusters:
+            for (name, rank, score) in ranked_clusters[cluster]:
+                f.write(",".join([str(cluster), str(rank), str(score)]) + ":" + str(name) + "\n")
 
 
 if __name__ == "__main__":
@@ -107,11 +128,11 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--type", type=str, required=True,
-        choices=["kmeans", "hierarchical"],
+        choices=["kmeans", "hierarchical", "optics", "dbscan"],
         help="Clustering technique to be used, one of kmeans and hierarchical", 
     )
     parser.add_argument(
-        "--num-clusters", type=int, help="Number of clusters", required=True,
+        "--num-clusters", type=int, help="Number of clusters",
     )
     parser.add_argument(
         "--linkage", type=str, help="Type of linkage in agglomerative clusering",
--- mocap_processing/viz/tsne.py
+++ mocap_processing/viz/tsne.py
@@ -1,26 +1,41 @@
-from sklearn.manifold import TSNE
+from sklearn.manifold import MDS, TSNE
 
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
 
+def normalize_features(features):
+    X = np.array(features)
+    Xmean, Xstd = X.mean(axis=0), X.std(axis=0)
+    return (X - Xmean) / (Xstd + 1.0e-8)
+
+
 def get_tsne_embeddings(X):
     X = np.array(X)
     X_embedded = TSNE(
-        n_components=2, init='random', random_state=0,
+        n_components=2, random_state=0
     ).fit_transform(X)
     return X_embedded
 
 
-def plot_tsne_embeddings(filename, X, labels=None):
-    fig, ax = plt.subplots()
+def get_mds_embeddings(X):
+    X = np.array(X)
+    X_embedded = MDS(
+        n_components=2, random_state=0,
+    ).fit_transform(X)
+    return X_embedded
+
+
+def plot_embeddings(filename, X, labels=None):
+    fig, ax = plt.subplots(figsize=(10, 7.5))
     if labels is None:
-        ax.scatter(X[:,0], X[:,1], marker=",")
+        ax.scatter(X[:,0], X[:,1], marker="o")
     else:
-        for l in np.unique(labels):
+        # In order of count of labels, highest to lowest
+        for l in np.flip(np.argsort(np.bincount(labels))):
             i = np.where(labels == l)
-            ax.scatter(X[i,0], X[i,1], label=l, marker=",")
+            ax.scatter(X[i,0], X[i,1], label=l, marker="o")
     ax.legend()
     plt.savefig(filename)
 
@@ -33,7 +48,11 @@ def main(args):
             filename, features = line.split(":")
             all_features.append(np.array(list(map(float, features.strip().split()))))
             filenames.append(filename.strip())
-    tsne_embeddings = get_tsne_embeddings(all_features)
+    norm_features = normalize_features(all_features)
+    if args.algorithm == "tsne":
+        embeddings = get_tsne_embeddings(norm_features)
+    elif args.algorithm == "mds":
+        embeddings = get_mds_embeddings(norm_features)
     filename2label = {}
     if args.clusters_file:
         with open(args.clusters_file) as f:
@@ -41,9 +60,9 @@ def main(args):
                 label_and_score, filename = line.split(":")
                 label = int(label_and_score.strip().split(",")[0])
                 filename2label[filename.strip()] = label
-    plot_tsne_embeddings(
+    plot_embeddings(
         filename=args.output_file, 
-        X=tsne_embeddings,
+        X=embeddings,
         labels=[
             filename2label[filename] for filename in filenames
         ] if args.clusters_file else None,
@@ -57,5 +76,6 @@ if __name__ == "__main__":
     parser.add_argument("--features-file", type=str, required=True)
     parser.add_argument("--output-file", type=str, required=True)
     parser.add_argument("--clusters-file", type=str)
+    parser.add_argument("--algorithm", choices=["mds", "tsne"], required=True)
     args = parser.parse_args()
     main(args)
