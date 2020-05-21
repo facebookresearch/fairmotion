f3f7e1013ada0239c628ee8e8f17c50fbdde5549
Deepak Gopinath <dgopinath@fb.com>
2020-05-20 17:48:48 -0700

    Adding comments explaining transformer architecture

--- mocap_processing/models/transformer.py
+++ mocap_processing/models/transformer.py
@@ -20,6 +20,7 @@ class STTransformerLayer(nn.Module):
         self.spatial_norm = LayerNorm(hidden_dim)
         self.spatial_dropout = nn.Dropout(p=dropout)
 
+        # The parameters of temporal attention MHA are separate for each joint.
         self.temporal_attn = [
             MultiheadAttention(
                 hidden_dim, attention_heads, dropout=dropout
@@ -38,6 +39,19 @@ class STTransformerLayer(nn.Module):
         self.window = window
 
     def _generate_square_subsequent_mask(self, sz):
+        """
+        The mask returned for size=4 looks like this
+        [
+            [0, -inf, -inf, -inf],
+            [0, 0, -inf, -inf],
+            [0, 0, 0, -inf],
+            [0, 0, 0, 0],
+        ]
+
+        The mask value is additive and is used in scaled dot product attention.
+        It is added to the dot product of query and key, before the tensor is
+        fed to the softmax
+        """
         mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
         mask = mask.float().masked_fill(
             mask == 0, float('-inf')
@@ -63,7 +77,9 @@ class STTransformerLayer(nn.Module):
         spatial_output = spatial_output.permute(1, 0, 2)
 
         # transpose to (num_joints, time, batch_size, embedding_size)
-        temporal_summary = x[max(0, t - self.window): max(t, self.window)].permute(2, 0, 1, 3)
+        temporal_summary = x[
+            max(0, t - self.window): max(t, self.window)
+        ].permute(2, 0, 1, 3)
         temporal_mask = self._generate_square_subsequent_mask(self.window).to(
             device=x.device,
         )
@@ -93,6 +109,22 @@ class STTransformerLayer(nn.Module):
 
 
 class STTransformer(nn.Module):
+    """
+    This is an implementation of the architecture from the paper 'Attention,
+    please: A Spatio-temporal Transformer for 3D Human Motion Prediction; Aksan
+    et al.' https://arxiv.org/abs/2004.08692
+
+    See Fig 2 for achitecture overview. The basic components of the model are:
+    * Joint embedding projection
+    * Positional encoding + Dropout
+    * Attention layer consisting of 
+        * Temporal attention block that updates a joint’s embedding by looking
+          at the past instances of the same joint
+        * Spatial block that attends over all the joints in the current frame
+        * Feed-forward network
+    * Embedding projection to joint space
+    * Residual connection from input to output
+    """
     def __init__(
         self, input_dim, hidden_dim=128, num_layers=4, dropout=0.1,
         attention_heads=8, feedforward_size=256, num_joints=24, window=120,
@@ -103,7 +135,9 @@ class STTransformer(nn.Module):
         self.window = window
         self.dropout = nn.Dropout(p=dropout)
         self.embedding = nn.Linear(input_dim, hidden_dim * num_joints)
-        self.pos_encoder = pe.PositionalEncoding(hidden_dim * num_joints, dropout)
+        self.pos_encoder = pe.PositionalEncoding(
+            hidden_dim * num_joints, dropout
+        )
         self.layers = _get_clones(
             STTransformerLayer(
                 hidden_dim, dropout, attention_heads, feedforward_size
@@ -125,14 +159,21 @@ class STTransformer(nn.Module):
         tgt = tgt.transpose(0, 1)
 
         if self.training:
+            """
+            During training, the concatenated source and target sequence forms
+            the input to the model. At each time step, the model predicts the
+            next pose autoregressively.
+            """
             concat_inputs = torch.cat((src, tgt))
+            # Target sequence is src[1:] + tgt[:]
             max_len = src.shape[0] + tgt.shape[0] - 1
             reshaped_inputs = self.dropout(
                 self.pos_encoder(self.embedding(concat_inputs))
             ).view(
                 max_len + 1, tgt.shape[1], self.num_joints, -1
             )
-
+            # We store autoregressive predictions by the model in 
+            # zero-initialized `outputs` tensor
             outputs = torch.zeros(max_len, src.shape[1], src.shape[2]).to(
                 src.device
             )
@@ -141,22 +182,31 @@ class STTransformer(nn.Module):
                 for layer in self.layers:
                     x = layer(x, t)
                 x = self.project_to_output(x)
+                # Attention layer represents pose with shape
+                # (num_joints, angle_dim). We need to flatten it.
                 x = x.view(self.window, src.shape[1], -1)
+                # window_t is sliding window index of time t
                 window_t = min(self.window - 1, t)
                 output = x[window_t] + concat_inputs[t]
                 outputs[t] = output
         else:
             if max_len is None:
+                # During inference, we are given source sequence and we predict
+                # only the target sequence.
                 max_len = tgt.shape[0]
             inputs = torch.zeros(
                 src.shape[0] + max_len - 1, src.shape[1], src.shape[2]
             ).to(src.device)
+            # Set source as input. We fill the rest of the zero tensor with
+            # predicted target poses iteratively later
             inputs[:src.shape[0]] = src
             outputs = torch.zeros(max_len, src.shape[1], src.shape[2]).to(
                 src.device
             )
             for tgt_t in range(max_len):
                 combined_t = src.shape[0] + tgt_t
+                # Bounds of sliding window of inputs that are passed to the
+                # attention layers
                 window_l, window_r = (
                     max(0, combined_t - self.window),
                     max(combined_t, self.window),
@@ -179,6 +229,7 @@ class STTransformer(nn.Module):
                 inputs[combined_t] = output
                 outputs[tgt_t] = output
 
+        # transpose back to (B, T, E) format
         return outputs.transpose(0, 1)
 
 
