006e7519216e51a19870cb68059f7eadbb0b39c4
Deepak Gopinath <dgopinath@fb.com>
2020-06-22 11:25:33 -0700

    Remove irrelevant tools folder

--- mocap_processing/tools/crawler/move_files.sh
+++
@@ -1,9 +0,0 @@
-file_with_filenames=$1
-output_folder=$2
-cat $file_with_filenames | while read file
-do
-	folder=$(echo "$file" | rev | cut -f2,3,4 -d/ | rev)
-	echo $folder
-	mkdir -p $output_folder/"$folder"
-	cp "$file".bvh $output_folder/"$folder"
-done
