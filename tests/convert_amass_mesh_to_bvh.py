e1e589ce187397d237877f4754c699dda4639723
visonpon <visonpon@gmail.com>
2022-04-25 10:44:51 +0800

    Update convert_amass_mesh_to_bvh.py

--- tests/convert_amass_mesh_to_bvh.py
+++ tests/convert_amass_mesh_to_bvh.py
@@ -14,13 +14,13 @@ def load_body_model(bm_path, num_betas=10, model_type="smplh"):
         # model_type=model_type
     ).to(comp_device)
     return bm
+ 
+file='./*.npz'#TODO --- file from amass
+bm_path = '../smplh/male/model.npz'
+num_betas=10 #TODO --- set tp 16 with hand but this tool doesn't support hand
+model_type='smplh'
+bm = load_body_model(bm_path,num_betas,model_type)
   
-  file='./*.npz'#TODO --- file from amass
-  bm_path = '../smplh/male/model.npz'
-  num_betas=10 #TODO --- set tp 16 with hand but this tool doesn't support hand
-  model_type='smplh'
-  bm = load_body_model(bm_path,num_betas,model_type)
-  
-  motion = amass.load(file,bm,bm_path,model_type)
-  bvh.save(motion,filename='./result.bvh')
+motion = amass.load(file,bm,bm_path,model_type)
+bvh.save(motion,filename='./result.bvh')
   
